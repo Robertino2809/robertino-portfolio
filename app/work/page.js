99ea8d07027e2b84248084b1396877b830764d8d
Robertino2809 <117544247+Robertino2809@users.noreply.github.com>
2024-08-26 11:41:31 +0200

    Commiting changes

--- app/work/page.js
+++ app/work/page.js
@@ -28,7 +28,7 @@ const skills = {
 const projects = [
   {
     num: "1",
-    category: "full-stack",
+    category: "Full-stack",
     title: "Print Alfa Web Shop",
     description: "This project is a comprehensive Full-stack e-commerce platform developed for a graphic design company using Next.js, React, and Tailwind CSS, and served as the final project for graduation. The platform features an admin dashboard where administrators can add, remove, and edit products, add and remove categories, you can also watch all your orders. On the front end, users can browse products, add items to their cart, and complete purchases through a Stripe-integrated checkout system. The platform also includes secure user authentication for login and registration, ensuring a smooth and secure shopping experience.",
     stack: [
@@ -44,7 +44,7 @@ const projects = [
   },
   {
     num: "2",
-    category: "frontend",
+    category: "Frontend",
     title: "Video Alfa website",
     description: "This project is a sleek and responsive web frontend developed for a local photo studio using React and Tailwind CSS. The platform offers a modern and user-friendly interface, allowing clients to easily browse through the studio's gallery and contact the studio. Leveraging React component-based architecture, the site ensures dynamic content updates and smooth user interactions. Tailwind CSS provides a utility-first approach to styling, enabling rapid and customized design that aligns with the studio's branding. This project highlights the ability to create visually appealing and functional web applications tailored to specific business needs.",
     stack: [
@@ -59,7 +59,7 @@ const projects = [
   },
   {
     num: "3",
-    category: "frontend",
+    category: "Frontend",
     title: "Apple website",
     description: "This project is a visually stunning and responsive web frontend designed as an Apple clone website, crafted with React, Tailwind CSS, Three.js, and GSAP animations. The platform offers a sophisticated and user-friendly interface, providing visitors with an immersive experience as they explore the site's features. Utilizing React's component-based architecture, the site ensures dynamic content updates and seamless user interactions. Tailwind CSS delivers a utility-first approach to styling, enabling rapid and customized design that mirrors Apple's clean and modern aesthetic. Three.js brings the site to life with engaging 3D graphics, while GSAP animations add fluid transitions and interactive elements.",
     stack: [
@@ -74,6 +74,19 @@ const projects = [
   {
     num: "4",
     category: "UI/UX",
+    title: "Foodoo Mobile App Design",
+    description: "Foodoo is a dynamic app that seamlessly blends the art of healthy cooking with the convenience of meal delivery, designed to support your nutritious lifestyle. It offers a wide array of curated recipes catering to various dietary preferences, from vegetarian and vegan to gluten-free and low-carb, while also allowing you to order freshly prepared, nutrient-rich meals delivered right to your doorstep. With personalized meal planning, in-app nutrition tracking, and easy ingredient shopping, Foodoo makes it simple to maintain a balanced diet. The app also features cooking tutorials, community engagement, and tools to manage dietary restrictions, making healthy eating both accessible and enjoyable.",
+    stack: [
+      { icon: <FaFigma />, name: "figma" },
+      { icon: <SiAdobephotoshop />, name: "adobe photoshop" },
+    ],
+    image: "/assets/work/thumbnail5.png",
+    live: "https://www.behance.net/gallery/204361377/Foodoo-Mobile-App",
+    github: "",
+  },
+  {
+    num: "5",
+    category: "UI/UX",
     title: "Vibes Mobile App Design",
     description: "This UI/UX design project was crafted for a Fiverr client and is centered on Vibes Mobile App, a unique social media platform where users can listen to music, buy cryptocurrency, and reward others with crypto for their musical creations. The app also features an integrated crypto wallet, enabling seamless financial transactions within the platform. The design emphasizes a user-friendly and visually engaging interface that enhances the user experience, making navigation intuitive and enjoyable. Each aspect of the app, from the music player to the crypto wallet, was meticulously designed to ensure both functionality and aesthetic appeal, providing users with a seamless and immersive experience.",
     stack: [
@@ -81,10 +94,11 @@ const projects = [
       { icon: <SiAdobephotoshop />, name: "adobe photoshop" },
     ],
     image: "/assets/work/thumbnail3.png",
-    live: "https://dribbble.com/shots/24538913-Vibes-Mobile-App?added_first_shot=true",
+    live: "https://www.behance.net/gallery/204383053/Music-App",
     github: "",
   },
 ];
+
 
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
@@ -103,32 +117,26 @@ const Work = () => {
       }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
-      <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row xl:gap-[30px] h-[50%]">
-          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div className="flex flex-col gap-[30px]">
-              <div className="text-6xl font-extrabold text-secondary">{project.num}</div>
-              <div>
-                <h2 className="text-[32px] font-bold text-third group-hover:text-secondary transition-all duration-500 capitalize">
-                  {project.title}
-                </h2>
-                <h2 className="text-[22px] font-bold text-zinc-600 group-hover:text-secondary transition-all duration-500 capitalize mb-2">
-                  {project.category} project
-                </h2>
-                <p className="text-zinc-600">{project.description}</p>
-                <ul className="flex gap-4 mt-4 mb-4">
-                  {project.stack.map((item, index) => {
-                    return (
-                      <li key={index} className="text-4xl text-third hover:text-secondary">
-                        {item.icon}
-                      </li>
-                    );
-                  })}
-                </ul>
-                <div className="border border-stone-300"></div>
+        <section>
+          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+              <div className="lg:py-24">
+                <h2 className="text-3xl font-bold sm:text-4xl">{projects[0].title}</h2>
+                <h2 className="text-xl font-bold mt-5">{projects[0].category}</h2>
+
+                <p className="mt-4 text-gray-600">
+                  {projects[0].description}
+                </p>
+                <div className="flex gap-6 mt-4 mb-4">
+                  {projects[0]?.stack.map((stackItem, index) => (
+                    <div key={index} className="text-5xl">
+                      {stackItem.icon}
+                    </div>
+                  ))}
+                </div>
                 <div className="flex items-center gap-4 mt-2">
                   {/* live project button */}
-                  <Link href={project.live}>
+                  <Link href={projects[0].live}>
                     <TooltipProvider delayDuration={100}>
                       <Tooltip>
                         <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-[#ebebeb] flex justify-center items-center group">
@@ -155,37 +163,220 @@ const Work = () => {
                   </Link>
                 </div>
               </div>
+              <div className="relative h-52 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+                <img
+                  alt=""
+                  src="/assets/work/thumbnail1.png"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%]">
-            <Swiper
-              spaceBetween={30}
-              slidesPerView={1}
-              className="xl:h-[520px] mb-12"
-              onSlideChange={handleSlideChange}
-            >
-              {projects.map((project, index) => {
-                return (
-                  <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-[#ebebeb]">
-                      {/* overlay */}
-                      <div className="absolute top-0 bottom-0 w-full h-full z-10"></div>
-                      {/* image */}
-                      <div className="relative w-full h-full">
-                        <Image src={project.image} fill className="object-cover" alt="" />
-                      </div>
+        </section>
+        <section>
+          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+              <div className="lg:py-24">
+                <h2 className="text-3xl font-bold sm:text-4xl">{projects[1].title}</h2>
+                <h2 className="text-xl font-bold mt-5">{projects[1].category}</h2>
+
+                <p className="mt-4 text-gray-600">
+                  {projects[1].description}
+                </p>
+                <div className="flex gap-6 mt-4 mb-4">
+                  {projects[1]?.stack.map((stackItem, index) => (
+                    <div key={index} className="text-5xl">
+                      {stackItem.icon}
                     </div>
-                  </SwiperSlide>
-                );
-              })}
-              {/* buttons */}
-              <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles="bg-[#ebebeb] text-third text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all rounded-full" />
-            </Swiper>
+                  ))}
+                </div>
+                <div className="flex items-center gap-4 mt-2">
+                  {/* live project button */}
+                  <Link href={projects[1].live}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-[#ebebeb] flex justify-center items-center group">
+                          <BsArrowUpRight className="text-3xl group-hover:text-secondary" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Live project</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                  {/* github project button */}
+                  <Link href={project.github}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-[#ebebeb] flex justify-center items-center group">
+                          <BsGithub className="text-3xl group-hover:text-secondary" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Github repository</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                </div>
+              </div>
+              <div className="relative h-52 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+                <img
+                  alt=""
+                  src="/assets/work/thumbnail2.png"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </section>
+        <section>
+          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+              <div className="lg:py-24">
+                <h2 className="text-3xl font-bold sm:text-4xl">{projects[2].title}</h2>
+                <h2 className="text-xl font-bold mt-5">{projects[2].category}</h2>
+
+                <p className="mt-4 text-gray-600">
+                  {projects[2].description}
+                </p>
+                <div className="flex gap-6 mt-4 mb-4">
+                  {projects[2]?.stack.map((stackItem, index) => (
+                    <div key={index} className="text-5xl">
+                      {stackItem.icon}
+                    </div>
+                  ))}
+                </div>
+                <div className="flex items-center gap-4 mt-2">
+                  {/* live project button */}
+                  <Link href={projects[2].live}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-[#ebebeb] flex justify-center items-center group">
+                          <BsArrowUpRight className="text-3xl group-hover:text-secondary" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Live project</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                  {/* github project button */}
+                  <Link href={project.github}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-[#ebebeb] flex justify-center items-center group">
+                          <BsGithub className="text-3xl group-hover:text-secondary" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Github repository</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                </div>
+              </div>
+              <div className="relative h-52 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+                <img
+                  alt=""
+                  src="/assets/work/thumbnail4.png"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+              <div className="lg:py-24">
+                <h2 className="text-3xl font-bold sm:text-4xl">{projects[3].title}</h2>
+                <h2 className="text-xl font-bold mt-5">{projects[3].category}</h2>
+
+                <p className="mt-4 text-gray-600">
+                  {projects[3].description}
+                </p>
+                <div className="flex gap-6 mt-4 mb-4">
+                  {projects[3]?.stack.map((stackItem, index) => (
+                    <div key={index} className="text-5xl">
+                      {stackItem.icon}
+                    </div>
+                  ))}
+                </div>
+                <div className="flex items-center gap-4 mt-2">
+                  {/* live project button */}
+                  <Link href={projects[3].live}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-[#ebebeb] flex justify-center items-center group">
+                          <BsArrowUpRight className="text-3xl group-hover:text-secondary" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Live project</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                </div>
+              </div>
+              <div className="relative h-52 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+                <img
+                  alt=""
+                  src="/assets/work/thumbnail5.png"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+            <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+              <div className="lg:py-24">
+                <h2 className="text-3xl font-bold sm:text-4xl">{projects[4].title}</h2>
+                <h2 className="text-xl font-bold mt-5">{projects[4].category}</h2>
+
+                <p className="mt-4 text-gray-600">
+                  {projects[4].description}
+                </p>
+                <div className="flex gap-6 mt-4 mb-4">
+                  {projects[4]?.stack.map((stackItem, index) => (
+                    <div key={index} className="text-5xl">
+                      {stackItem.icon}
+                    </div>
+                  ))}
+                </div>
+                <div className="flex items-center gap-4 mt-2">
+                  {/* live project button */}
+                  <Link href={projects[4].live}>
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-[#ebebeb] flex justify-center items-center group">
+                          <BsArrowUpRight className="text-3xl group-hover:text-secondary" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Live project</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                </div>
+              </div>
+              <div className="relative h-52 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+                <img
+                  alt=""
+                  src="/assets/work/thumbnail3.png"
+                  className="absolute inset-0 h-full w-full object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
     </motion.section>
   );
 };
 
 export default Work;
+
+
+
+
